476fe0f5f7a3714d52a085ee62ec5cca4ae0a4ad
Henry Baxter <henry.baxter@gmail.com>
2016-11-25 10:18:08 -0800

    Print all reports

--- urls.py
+++ urls.py
@@ -6,8 +6,12 @@ bucket = boto3.resource('s3').Bucket('xcite-simulations')
 parser = argparse.ArgumentParser()
 parser.add_argument('--name', required=True)
 args = parser.parse_args()
+base_url = 'https://s3-us-west-2.amazonaws.com/xcite-simulations/{}'
 
+reports = []
 for item in bucket.objects.all():
+    if item.key.endswith('report.pdf'):
+        reports.append(base_url.format(item.key))
     if not item.key.startswith(args.name):
         continue
     if item.key.endswith('converted-to.pdf') or item.key.endswith('.eps'):
@@ -16,3 +20,4 @@ for item in bucket.objects.all():
         continue
     print('https://s3-us-west-2.amazonaws.com/xcite-simulations/{}'.format(item.key))
 
+[print(report) for report in reports]
