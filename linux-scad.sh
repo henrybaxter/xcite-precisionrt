b1ee098bda3033327d8ba16230095b322bcb2fd4
Henry Baxter <henry.baxter@gmail.com>
2016-12-20 09:51:44 -0800

    Scad exporter

---
+++ linux-scad.sh
@@ -0,0 +1,3 @@
+openscad $1.scad --camera=-0,3.60,4.92,90.0,0.0,0.0,62.09 -o $1_side.png --imgsize=1600,600 --autocenter && open $1_side.png
+openscad $1.scad --camera=-0,0,5,0.0,0.0,0.0,12 -o $1_phantom.png --imgsize=1600,600 --autocenter && open $1_phantom.png
+openscad $1.scad --camera=-0,0,5,180.0,0.0,0.0,6 -o $1_anode.png --imgsize=1600,600 --autocenter && open $1_anode.png
