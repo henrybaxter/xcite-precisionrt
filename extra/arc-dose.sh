4739b521d97d93852adf0e62dfbb7a1b207e987a
Henry Baxter <henry.baxter@gmail.com>
2017-01-05 08:39:30 -0800

    extra

---
+++ extra/arc-dose.sh
@@ -0,0 +1,4 @@
+rm ~/projects/EGSnrc/egs_home/dosxyznrc/try-arc*
+cp try-arc.egsinp ~/projects/EGSnrc/egs_home/dosxyznrc/
+dosxyznrc -p allkV -i try-arc.egsinp > dose-arc.egslst
+cp ~/projects/EGSnrc/egs_home/dosxyznrc/try-arc.3ddose .
