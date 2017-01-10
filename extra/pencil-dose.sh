eb234f7e7a3c316ad6f69521268b2ce999284fb2
Henry Baxter <henry.baxter@gmail.com>
2017-01-10 11:10:28 -0800

    Cleanup

--- extra/pencil-dose.sh
+++
@@ -1,4 +0,0 @@
-rm ~/projects/EGSnrc/egs_home/dosxyznrc/try-pencil*
-cp try-pencil.egsinp ~/projects/EGSnrc/egs_home/dosxyznrc/
-dosxyznrc -p allkV -i try-pencil.egsinp > pencil-dose.egslst
-cp ~/projects/EGSnrc/egs_home/dosxyznrc/try-pencil.3ddose .
--- sim.sh
+++
@@ -1,1 +0,0 @@
-python3 simulate.py "$1" --dose-recycle=999 --collimator=$2 --rotate --histories=10000000000
