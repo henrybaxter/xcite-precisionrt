cffd0ad84a072d474ac5b64cb561f5062a2756f0
Henry Baxter <henry.baxter@gmail.com>
2017-01-16 19:17:11 +0000

    Clean

---
+++ clean.sh
@@ -0,0 +1,5 @@
+rm ~/EGSnrc/egs_home/dosxyznrc/*.egslst
+rm ~/EGSnrc/egs_home/dosxyznrc/*.errors
+rm ~/EGSnrc/egs_home/dosxyznrc/*.egsinp
+rm ~/EGSnrc/egs_home/dosxyznrc/*.3ddose
+rm ~/EGSnrc/egs_home/dosxyznrc/*.npz
