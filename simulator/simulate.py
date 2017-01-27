414918e647f6ce87316833e6015bd76b356535f9
Henry Baxter <henry.baxter@gmail.com>
2017-01-27 13:21:02 -0500

    oops

--- simulator/simulate.py
+++ simulator/simulate.py
@@ -297,7 +297,7 @@ async def dose_combine(doses, weights=None):
     inputs = [d['3ddose'] for d in doses]
     if weights is None:
         weights = np.full(len(doses), 1 / len(doses))
-    inputs += weights
+    inputs += list(weights)
     s = 'combinedv4' + json.dumps(inputs)
     base = hashlib.md5(s.encode('utf-8')).hexdigest()
     os.makedirs('combined', exist_ok=True)
